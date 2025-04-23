9d0c9327dbc32c674628ff9f8148f078323e5b9f
fanwin <41528915+fanwin@users.noreply.github.com>
2025-04-23 17:33:33 +0800

    update description of this project.

---
+++ README.md
@@ -0,0 +1,11 @@
+😊 我能实现和大模型的对话啦
+
+![img.png](statics/img.png)
+
+🤖 和大厂的模型聊天岂不是更香，看看deepseek、豆包、moonshot多厉害
+
+😊 除了满足代码欲，还有其他的目的哦 🚀
+
+
+❤️ 我可以支持上传文件聊天了，对啦，支持几乎任意类型的文件哦：PDF、Markdown、Excel、Word、PPT、TXT、JSON、YAML、HTML、、、
+![img_4.png](statics/img_4.png)
--- rag_kernel/utils.py
+++ rag_kernel/utils.py
@@ -5,10 +5,7 @@ from openai import OpenAI
 from utils.settings import remote_moonshot_llm
 
 def extract_text_from_llm(file_path):
-    client = OpenAI(
-        api_key="sk-IrjHqno9H4w0LFhpac33IvzUBRceMapE6I76brB1WaDCPSLd",
-        base_url="https://api.moonshot.cn/v1",
-    )
+    client = remote_moonshot_llm()
     file_object = client.files.create(file=Path(file_path), purpose="file-extract")
     file_content = client.files.content(file_id=file_object.id).json()
 
